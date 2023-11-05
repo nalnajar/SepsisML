cefeb48ac3e4a51c42f46e4982f75fcaaf884d27
nalnajar <n_al-najar@fanshaweonline.ca>
2023-11-04 21:57:46 -0400

    Added 9 day test

--- SepsisML/venv/test.py
+++ SepsisML/venv/test.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 # Load the data from "sepsis_survival_primary_cohort.csv"
 load_data = pd.read_csv('csv/sepsis_survival_primary_cohort.csv')
 
-# Select features and target variable
+# Select features and the target variable
 features = load_data[['age_years', 'sex_0male_1female', 'episode_number']]
 target = load_data['hospital_outcome_1alive_0dead']
 
@@ -27,27 +27,66 @@ age = float(input("Enter age: "))
 sex = int(input("Enter sex (0 for male, 1 for female): "))
 episode_number = int(input("Enter episode number: "))
 
+# Number of days untreated (you can adjust this as needed)
+days_untreated = int(input("Enter number of days untreated: "))
+
+# Calculate the daily increased risk based on your range (4-9% per hour)
+daily_increased_risk_range = (0.04, 0.09)
+daily_increased_risk_per_day = sum(
+    daily_increased_risk_range) / 2  # Average risk per hour
+
 # Prepare the user's input as a feature vector
 user_input = [[age, sex, episode_number]]
 
-# Make a prediction for the user's input
-prediction = regressor.predict(user_input)
+# Make predictions for the user's input
+immediate_prediction = regressor.predict(user_input)
 
-print(f"Prediction: {prediction}\n\n")
+# Predicting after 9 days (you may need to adjust this)
+episode_number_after_9_days = episode_number + 9
+user_input_after_9_days = [[age, sex, episode_number_after_9_days]]
+prediction_after_9_days = regressor.predict(user_input_after_9_days)
 
-# Interpret the prediction
-if prediction > 0.5:
-    result = "alive"
+# Interpret the predictions
+
+
+def interpret_prediction(prediction):
+    if prediction > 0.5:
+        return "alive"
+    else:
+        return "dead"
+
+
+immediate_result = interpret_prediction(immediate_prediction)
+result_after_9_days = interpret_prediction(prediction_after_9_days)
+
+# Calculate the odds of dying within 9 days
+average_survival_rate = 0.30  # 30% survival rate
+
+if immediate_prediction == 0:
+    odds_of_dying_immediately = 1.0  # Certain death if immediate prediction is "dead"
 else:
-    result = "dead"
+    odds_of_dying_immediately = 1 - \
+        (1 - average_survival_rate) ** days_untreated
 
-# Print the prediction to the screen
+if prediction_after_9_days == 0:
+    # Certain death if prediction after 9 days is "dead"
+    odds_of_dying_after_9_days = 1.0
+else:
+    odds_of_dying_after_9_days = 1 - \
+        (1 - average_survival_rate) ** (9 + days_untreated)
+
+# Print the predictions and odds to the screen
 print("Predicting hospital outcome based on the provided data:")
 print(f"Age: {age}")
 print(f"Sex (0 for male, 1 for female): {sex}")
 print(f"Episode number: {episode_number}")
+print(f"Number of days untreated: {days_untreated} days")
 print(
-    f"Based on the provided data, the individual is predicted to be {result}.\n")
+    f"Based on the provided data, the individual is predicted to be {immediate_result} immediately.\n")
+print(
+    f"Odds of dying after {days_untreated} days: {odds_of_dying_immediately:.2%}")
+print(
+    f"Odds of dying within 9 days if left untreated: {odds_of_dying_after_9_days:.2%}\n")
 
 # Calculate Mean Squared Error and R-squared on the test data
 y_pred = regressor.predict(X_test)
@@ -58,21 +97,12 @@ r2 = r2_score(y_test, y_pred)
 alive_count_test = sum(y_test)
 dead_count_test = len(y_test) - alive_count_test
 
-# Count the number of alive and dead individuals in the training data
-alive_count_train = sum(y_train)
-dead_count_train = len(y_train) - alive_count_train
-
 # Print the counts of alive and dead individuals in test and training data
 print(f"Number of individuals in the test data: {len(y_test)}")
 print(
     f"Number of individuals predicted as alive (test data): {alive_count_test}")
 print(
     f"Number of individuals predicted as dead (test data): {dead_count_test}")
-print(f"\nNumber of individuals in the training data: {len(y_train)}")
-print(
-    f"Number of individuals predicted as alive (training data): {alive_count_train}")
-print(
-    f"Number of individuals predicted as dead (training data): {dead_count_train}")
 
 # Calculate the number of episodes for each individual in the test data
 episode_counts = X_test['episode_number'].values
@@ -114,6 +144,7 @@ plot_tree(regressor, feature_names=features.columns,
           filled=True, rounded=True, fontsize=6)
 
 plt.show()
+
 
 # Create a new DataFrame for the test data
 test_data = X_test.copy()
